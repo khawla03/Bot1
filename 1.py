4bd9b03a4ceb1cd2a86e10335897051eaec47a96
MOUHEB khaoula <ik_mouheb@esi.dz>
2021-05-29 12:43:15 +0100

    final but not final

--- 1.py
+++ 1.py
@@ -4,11 +4,12 @@ import random
 import asyncio
 from discord import reaction
 
-
+embedVar = discord.Embed(title="LOG", description= ' log' , color=0x00ff00)
 client = discord.Client()
 lst = []
 log=[] 
 point=''
+logid=0
 #creds = None
 #service = build('docs', 'v1', credentials=creds)
 @client.event
@@ -49,17 +50,21 @@ async def on_message(message):
 
 @client.event
 async def on_message(message):
+    
+    i=0
+    channelid = client.get_channel(848122485806727168)
+    
     if message.content.startswith('$meet'):
         msg = message.content 
         i = msg.find('/')
         j = msg.find('at')
         k = msg.find('till')
         print(i)
-        embedVar = discord.Embed(title="Meeting", description= message.channel.name + ' meeting , react with ✅' , color=0x00ff00)
-        embedVar.add_field(name="Date", value=msg[i-2:i+8], inline=False)
-        embedVar.add_field(name="starts", value=msg[j:j+7], inline=False)
-        embedVar.add_field(name="till", value=msg[k+4:], inline=False)
-        await message.channel.send(embed=embedVar)
+        embedmeet = discord.Embed(title="Meeting", description= message.channel.name + ' meeting , react with ✅' , color=0x00ff00)
+        embedmeet.add_field(name="Date", value=msg[i-2:i+8], inline=False)
+        embedmeet.add_field(name="starts", value=msg[j:j+7], inline=False)
+        embedmeet.add_field(name="till", value=msg[k+4:], inline=False)
+        await message.channel.send(embed=embedmeet)
 
         #insert_field_at(index, *, name, value, inline=True)
         channel = message.channel
@@ -79,9 +84,8 @@ async def on_message(message):
     else:
       msg = message.content
       log =[]
-      i=0
-      channelid = client.get_channel(848122485806727168)
-      embedVar = discord.Embed(title="LOG", description= message.channel.name + ' log' , color=0x00ff00)
+      
+      
           
       if msg.startswith('$c_log'): 
           Response ='write the points you will talk about in this meeting:' 
@@ -101,11 +105,23 @@ async def on_message(message):
             point=msg.content
             print(point)
             i=i+1
+      
      
-          await channelid.send(embed=embedVar)
+          
+      else:
+         if msg.startswith('$add'): 
+           
+           embedVar.add_field(name='point'+str(i+1), value=msg[4:], inline=False)
+         else:
+            if msg.startswith('$s_log'):
+              await channelid.send(embed=embedVar)
+              embedVar.clear_fields()
+              p=i+1
+
+      
+           
 
 
 
 client.run(os.getenv('TOKEN'))
 
-#$create 12/12/2020 8pm
