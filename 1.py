5f8e9f36bf6583d00d2f7141214666579fc76d5d
Lakehal_AA <ia_lakehal@esi.dz>
2021-05-29 13:23:16 +0100

    Integration Backend

--- 1.py
+++ 1.py
@@ -2,65 +2,88 @@ import discord
 import os
 import random
 import asyncio
+import requests
 from discord import reaction
 
-embedVar = discord.Embed(title="LOG", description= ' log' , color=0x00ff00)
+embedVar = discord.Embed(title="LOG", description=' log', color=0x00ff00)
 client = discord.Client()
 lst = []
-log=[] 
-point=''
-logid=0
-#creds = None
-#service = build('docs', 'v1', credentials=creds)
+log = []
+point = ''
+logid = 0
+# creds = None
+# service = build('docs', 'v1', credentials=creds)
+
+
+url = 'https://warm-plateau-39485.herokuapp.com/meetings'
+myMeeting = {'titre': "S2EE meeting",
+             'days': 16,
+             'month': 8,
+             'heureInit': 17,
+             'minuteInit': 00,
+             'heureFin': 19,
+             'minuteFin': 00,
+             'members': ["7med", "Wisseme", "Mouna"],
+             'pointsAbord': ["Le lieu", "La stratégie"],
+             'rapporteur': "7med",
+             'chaineId':  "#106546",
+             'categNom': "S2EE 8) 8)",
+             'coordId':  "#1504"}
+
+
 @client.event
 async def on_ready():
-  print('We have logged in as {0.user}'.format(client))
+    print('We have logged in as {0.user}'.format(client))
+
 
 @client.event
 async def on_message(message):
-  msg = message.content
- 
-  if msg.startswith('$create'):
-    date = msg[8:18]
-    time = msg[19:]
-    str=date+time
-    await message.channel.send(str)
-    if str not in lst: 
-      lst.append(str)
-      print(lst)
-      await message.channel.send('!create meeting on ' + date + ' at ' + time)
+    msg = message.content
+
+    if msg.startswith('$create'):
+        date = msg[8:18]
+        time = msg[19:]
+        str = date+time
+        await message.channel.send(str)
+        if str not in lst:
+            lst.append(str)
+            print(lst)
+            await message.channel.send('!create meeting on ' + date + ' at ' + time)
+        else:
+            await message.channel.send('a meeting on that timing already exists')
     else:
-      await message.channel.send('a meeting on that timing already exists') 
-  else:
-   if msg.startswith('$pv'):
-     title = 'pv'
-     body = {
-      'title': title
-     }
-    
-     #doc = service.documents() \
-     #.create(body=body).execute()
-     # print('Created document with title: {0}'.format(
-     # doc.get('title')))
-     await message.channel.send('pv created')
-   else:
-     if msg.startswith('$meet'):
-       await message.channel.send('pv created')
-       
+        if msg.startswith('$pv'):
+            title = 'pv'
+            body = {
+                'title': title
+            }
+
+            # doc = service.documents() \
+            # .create(body=body).execute()
+            # print('Created document with title: {0}'.format(
+            # doc.get('title')))
+            await message.channel.send('pv created')
+        else:
+            if msg.startswith('$meet'):
+                await message.channel.send('pv created')
+
 
 @client.event
 async def on_message(message):
-    
-    i=0
+
+    i = 0
     channelid = client.get_channel(848122485806727168)
-    
+
     if message.content.startswith('$meet'):
-        msg = message.content 
+        msg = message.content
         i = msg.find('/')
         j = msg.find('at')
         k = msg.find('till')
+        x = requests.post(url, data=myMeeting)
+        print(x.text)
         print(i)
-        embedmeet = discord.Embed(title="Meeting", description= message.channel.name + ' meeting , react with ✅' , color=0x00ff00)
+        embedmeet = discord.Embed(
+            title="Meeting", description=message.channel.name + ' meeting , react with ✅', color=0x00ff00)
         embedmeet.add_field(name="Date", value=msg[i-2:i+8], inline=False)
         embedmeet.add_field(name="starts", value=msg[j:j+7], inline=False)
         embedmeet.add_field(name="till", value=msg[k+4:], inline=False)
@@ -68,8 +91,9 @@ async def on_message(message):
         channelmeet = client.get_channel(848164881663787039)
         await channelmeet.send(embed=embedmeet)
 
-        #insert_field_at(index, *, name, value, inline=True)
+        # insert_field_at(index, *, name, value, inline=True)
         channel = message.channel
+
         def check(reaction, user):
             return user == message.author and str(reaction.emoji) == '✅'
 
@@ -82,48 +106,42 @@ async def on_message(message):
             rapporteur = random.choice(users)
             await message.channel.send('le rapporteur : '+rapporteur.mention)
 
-        #await message.channel.send('{} has won the raffle.'.format(winner))
+        # await message.channel.send('{} has won the raffle.'.format(winner))
     else:
-      msg = message.content
-      log =[]
-      
-      
-          
-      if msg.startswith('$c_log'): 
-          Response ='write the points you will talk about in this meeting:' 
-          await message.channel.send(Response)
-          point='' 
-          def check(m):
+        msg = message.content
+        log = []
+
+        if msg.startswith('$c_log'):
+            Response = 'write the points you will talk about in this meeting:'
+            await message.channel.send(Response)
+            point = ''
+
+            def check(m):
                 if m.content != '**':
-                  log.append( m.content )
-                  embedVar.add_field(name='________________', value=log[i], inline=False)
+                    log.append(m.content)
+                    embedVar.add_field(name='________________',
+                                       value=log[i], inline=False)
                 return True
 
-          while (point != '**'):
-            channel = message.channel
+            while (point != '**'):
+                channel = message.channel
 
-            msg = await client.wait_for('message', check=check)
-            #await channel.send('hi2') 
-            point=msg.content
-            print(point)
-            i=i+1
-      
-     
-          
-      else:
-         if msg.startswith('$add'): 
-           
-           embedVar.add_field(name='________________', value=msg[4:], inline=False)
-         else:
-            if msg.startswith('$s_log'):
-              await channelid.send(embed=embedVar)
-              embedVar.clear_fields()
-              p=i+1
+                msg = await client.wait_for('message', check=check)
+                # await channel.send('hi2')
+                point = msg.content
+                print(point)
+                i = i+1
 
-      
-           
+        else:
+            if msg.startswith('$add'):
 
+                embedVar.add_field(name='________________',
+                                   value=msg[4:], inline=False)
+            else:
+                if msg.startswith('$s_log'):
+                    await channelid.send(embed=embedVar)
+                    embedVar.clear_fields()
+                    p = i+1
 
 
 client.run(os.getenv('TOKEN'))
-
